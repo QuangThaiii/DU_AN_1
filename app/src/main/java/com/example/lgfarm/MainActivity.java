b267e083f89b398ed31ad7c112771007f0e23a84
QuangThaiii <thaihht2002@gmail.com>
2021-11-05 22:29:07 +0700

    push 2

--- app/src/main/java/com/example/lgfarm/MainActivity.java
+++ app/src/main/java/com/example/lgfarm/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         String add="add thanh cong";
+        String add2="test push";
     }
 }
